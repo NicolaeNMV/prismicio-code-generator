dd6563a8a0b547f5cbdf3e5f8f94a5971ce2234c
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-01-31 13:19:22 +0100

    parse JSON and rename on-click function

--- src/pcg.cljs
+++ src/pcg.cljs
@@ -6,20 +6,21 @@
   (:use-macros
     [dommy.macros :only [node sel sel1]]))
 
-(defn log [& args]
-  (.log js/console (apply pr-str args)))
+(defn log [& args] (.log js/console (apply pr-str args)))
+(defn log-obj [obj] (.log js/console obj))
 
-(defn log-obj [obj]
-  (.log js/console obj))
+; Implement me!
+(defn generate [mask]
+  (str "source code for " mask))
 
-(def source      (dom/get-element "source"))
-(def destination (dom/get-element "destination"))
+(defn on-click []
+  (let [
+        json (dommy/value (sel1 :#input))
+        mask (js->clj (JSON/parse json))
+        code (generate mask)
+        ]
+    (log code)))
 
 (defn ^:export start []
-  (dommy/listen! (sel1 :#generate) :click generate)
+  (dommy/listen! (sel1 :#generate) :click on-click)
   (generate))
-
-(defn generate []
-  (let [json-string (:value (sel1 :#input))]
-    (log json-string)
-    (log (sel1 :#input))))
